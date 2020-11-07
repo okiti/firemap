a56a25059cbc9e0d0d0408a2876e1b0a1afe1f04
okiti <osivwiokiti@gmail.com>
2020-11-07 13:17:53 +0100

    Fix: Added dummy data

--- src/components/maps.vue
+++ src/components/maps.vue
@@ -6,18 +6,50 @@
 <script>
 import mapboxgl from 'mapbox-gl';
 import dayjs from 'dayjs';
-import axios from 'axios';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import pulse from '../lib/pulse';
 import redPulse from '../lib/redPulse';
-import yellowPulse from '../lib/yellowPulse';
-
 /* eslint-disable */
 export default {
   components: {},
   data() {
     return {
       map: false,
+      sensors: [
+        {
+          icon: 'pulsing-dot',
+          id: 43490203,
+          networkStatus: true,
+          fireStatus: false,
+          reconnectionTime: '2020-10-07T12:12:45+01:00',
+          ownerName: 'Joe Biden',
+          ownerNumber: '+23458882934343',
+          longitude: 5.2,
+          latitude: 7.25,
+        },
+        {
+          icon: 'redpulsing-dot',
+          id: 43490203,
+          networkStatus: true,
+          fireStatus: true,
+          reconnectionTime: '2020-10-07T12:12:45+01:00',
+          ownerName: 'Gene Plus',
+          ownerNumber: '+23458882934343',
+          longitude: 3.3,
+          latitude: 6.5,
+        },
+        {
+          icon: 'pulsing-dot',
+          id: 43490203,
+          networkStatus: true,
+          fireStatus: false,
+          ownerName: 'Micheal Akpan',
+          reconnectionTime: '2020-10-07T12:12:45+01:00',
+          ownerNumber: '+23458882934343',
+          longitude: 4.7,
+          latitude: 6.2,
+        },
+      ],
       sensorGeoJson: {
         type: 'geojson',
         data: {
@@ -31,32 +63,21 @@ export default {
   },
   async mounted() {
     const vm = this;
-    axios.get('http://fire-device.us-east-2.elasticbeanstalk.com/devices')
-    .then(async (response) => {
-      const devices = response.data.data;
-      console.log(devices);
-      await this.generateGeoJson(devices);
-    })
-
+    await this.generateGeoJson(vm.sensors);
     mapboxgl.accessToken = this.token;
-
     // const el = document.createElement('div');
     // el.className = 'marker';
-
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/osivwi/ckfgzpsf12odv19p8stu3aif0',
-      center: [49.943129, -4.380302],
-      zoom: 4.15,
+      center: [5.2, 7.25],
+      zoom: 6.15,
     });
-
     const pulsingDot = pulse(map);
     const redPulsingDot = redPulse(map);
-    const yellowPulsingDot = yellowPulse(map);
     map.on('load', () => {
       map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
       map.addImage('redpulsing-dot', redPulsingDot, { pixelRatio: 2 });
-      map.addImage('yellowpulsing-dot', yellowPulsingDot, { pixelRatio: 2 });
       map.addSource('places', vm.sensorGeoJson);
       // Add a layer showing the places.
       map.addLayer({
@@ -72,7 +93,6 @@ export default {
     map.on('mouseenter', 'places', () => {
       map.getCanvas().style.cursor = 'pointer';
     });
-
     map.on('mouseleave', 'places', () => {
       map.getCanvas().style.cursor = '';
     });
@@ -81,7 +101,6 @@ export default {
     map.on('click', 'places', (e) => {
       const coordinates = e.features[0].geometry.coordinates.slice();
       const { description } = e.features[0].properties;
-
       // Ensure that if the map is zoomed out such that multiple
       // copies of the feature are visible, the popup appears
       // over the copy being pointed to.
@@ -115,12 +134,12 @@ export default {
                 <p class="font-semibold text-xs"> Online since: ${dt} </p>
               </div>              
               <p class="font-bold pt-2 text-xl">${data[key].ownerName}</p>
-              <p class="text-sm pt-1 text-center px-5"></p>
+              <p class="text-sm pt-1 text-center px-5">Room 306</p>
               <div>
                 <a class="rounded-full flex justify-center items-center tel-holder" href="tel:${data[key].ownerNumber}"><i class="uil uil-calling text-2xl"></i></a>
               </div>
               </div>`,
-            icon: fireStatus ? 'pulsing-dot' : 'redpulsing-dot',
+            icon: `${data[key].icon}`,
           },
         };
         vm.sensorGeoJson.data.features.push(sensorData);
